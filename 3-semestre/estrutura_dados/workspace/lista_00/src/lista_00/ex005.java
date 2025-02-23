38b6976f821156509cfa083ddc3ca88068050ae0
Miguel Lyra <151979534+miguelfordev@users.noreply.github.com>
2025-02-23 17:46:32 -0300

    Update ex005.java

--- 3-semestre/estrutura_dados/workspace/lista_00/src/lista_00/ex005.java
+++ 3-semestre/estrutura_dados/workspace/lista_00/src/lista_00/ex005.java
@@ -1,5 +1,20 @@
 package lista_00;
-
+import java.util.Scanner;
 public class ex005 {
 
+	public static void main() {
+		String[] estoque = new String[5];
+		add(estoque);
+		System.out.println(estoque[1]);
+	}
+	
+	public static void add(String[] vt) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Digite o nome do produto, qtd min e qtd max, nessa ordem.");
+		vt[0] = sc.next();
+		vt[1] = sc.next();
+		vt[2] = sc.next();
+	}
+	
+	
 }
